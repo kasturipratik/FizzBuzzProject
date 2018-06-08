77dd10eff59b3d84c1cfc3ee71512281088dae71
kasturipratik <pratik.parajuli@yahoo.com>
2018-06-08 14:43:38 -0400

    Working version of FizzBuzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,13 @@
 package com.company;
 
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //calling array
-       String result [] = randomNum();
-       //printing array using a for loop
-        for (int i =0;i<result.length;i++){
-            System.out.println(result[i]);
+        // write your code here
+        String print[] =fizzBuzz();
+        for(int i =0; i <print.length;i++){
+            System.out.println(print[i]);
         }
-
-
     }
     public static String[] fizzBuzz(){
         String num[] = new String[100];
@@ -34,10 +28,6 @@ public class Main {
                     num[i-1]="Buzz";
                 }
             }
-            else if (i%7 ==0){
-                // multiple of seven
-                num[i-1] = String.valueOf(i)+ " - multiple of seven";
-            }
             else{
                 num[i-1] =String.valueOf(i);
             }
@@ -46,44 +36,5 @@ public class Main {
         //array returning
         return num;
 
-    }
-
-    public static String[] randomNum(){
-       //random number object
-        Random rnd = new Random();
-        int solution =0;
-        // generate random number and set it to variables
-        int beginning = 1+rnd.nextInt(100);
-        int ending = 1+rnd.nextInt(100);
-
-        //call fizzBuzz method to get the condition for fizz and buzz
-        String [] result = fizzBuzz();
-        //set the array to the size of the fizzBuzz array
-        String content[] = new String[result.length];
-//for loop to assign fizzBuzz values to content
-        for(int i =0;i <result.length;i++){
-            content[i] = result[i];
-        }
-        //condition to change the content elemen
-        if(beginning == ending){
-            content[0] = "Shazam";
-            content[content.length-1] = "Shazam";
-        }
-        else if((beginning - ending) <10 ){
-
-            if(beginning < ending){
-                int temp = beginning;
-                beginning = ending;
-                ending = temp;
-                solution = ending- beginning;
-            }
-            else{
-                solution = beginning - ending;
-            }
-            content[0] = "This won't take long - " + String.valueOf(solution);
-            content[content.length-1] =  "This won't take long - " +String.valueOf(solution);
-        }
-
-        return content;
     }
 }
